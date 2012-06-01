851c1006119ad3143362aa8aec69e05b913a04a4
Roland Warmerdam <rowno@webspirited.com>
2012-06-02 00:46:22 +1200

    Setup rendering and the basics of the main objects.

--- js/tetris.js
+++ js/tetris.js
@@ -6,42 +6,172 @@
 
     var CANVAS_WIDTH = 118,
         CANVAS_HEIGHT = 236,
-
+        PADDING = 2,
         $tetris = $('#tetris'),
-        $score = $tetris.find('.score'),
-
-        context = $tetris.find('canvas').get(0).getContext('2d'),
-        running = true,
-        score = 0;
+        Score,
+        Control,
+        Render;
 
 
-    context.fillStyle = '#000';
-    context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    function Block(color, x, y) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
 
-
-    function start() {
-        score = 0;
-        $score.text(score);
-        $tetris.addClass('running');
+        Block.blocks.push(this);
     }
 
-    function stop() {
-        $tetris.removeClass('running');
+    Block.blocks = [];
+    Block.width = 10;
+    Block.height = 10;
+
+    Block.prototype.draw = function (context) {
+        context.fillStyle = this.color;
+        context.fillRect(this.x, this.y, Block.width, Block.height);
+    };
+
+
+    function Shape(type, x, y, rotate) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.rotate = 0 || rotate;
+
+        this.blocks = [
+            new Block('red', this.x, this.y),
+            new Block('red', this.x + Block.width + PADDING, this.y),
+            new Block('red', this.x + Block.width + PADDING, this.y + Block.height + PADDING),
+            new Block('red', this.x + (Block.width + PADDING) * 2, this.y + Block.height + PADDING)
+        ];
     }
 
 
-    $tetris.off('click');
-    $tetris.on('click', function () {
-        if (running) {
-            stop();
-        } else {
-            start();
+    Score = (function () {
+        var exports = {},
+            score = 0,
+            $score = $tetris.find('.score');
+
+
+        function reset() {
+            score = 0;
+            $score.text(score);
+        }
+        exports.reset = reset;
+
+        return exports;
+    }());
+
+
+    Control = (function () {
+        var exports = {},
+            running = true;
+
+
+        function start() {
+            Score.reset();
+            Render.requestDraw();
+            $tetris.addClass('running');
+        }
+        exports.start = start;
+
+
+        function stop() {
+            $tetris.removeClass('running');
+        }
+        exports.stop = stop;
+
+
+        $tetris.off('click');
+        $tetris.on('click', function () {
+            if (running) {
+                stop();
+            } else {
+                start();
+            }
+
+            running = !running;
+        });
+
+        return exports;
+    }());
+
+
+    Render = (function () {
+        var exports = {},
+            context = $tetris.find('canvas').get(0).getContext('2d'),
+            queued = false,
+            drawing = false;
+
+
+        /**
+         * requestAnimationFrame polyfill
+         */
+        (function () {
+            var lastTime = 0,
+                vendors = ['ms', 'moz', 'webkit', 'o'],
+                x;
+
+            for (x = 0; x < vendors.length && !window.requestAnimationFrame; x += 1) {
+                window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+                window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
+                                              window[vendors[x] + 'CancelRequestAnimationFrame'];
+            }
+
+            if (!window.requestAnimationFrame) {
+                window.requestAnimationFrame = function (callback, element) {
+                    var currTime = new Date().getTime(),
+                        timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+                        id = window.setTimeout(
+                            function () { callback(currTime + timeToCall); },
+                            timeToCall
+                        );
+                    lastTime = currTime + timeToCall;
+                    return id;
+                };
+            }
+
+            if (!window.cancelAnimationFrame) {
+                window.cancelAnimationFrame = function (id) {
+                    clearTimeout(id);
+                };
+            }
+        }());
+
+
+        function draw() {
+            drawing = true;
+
+            window.requestAnimationFrame(function () {
+                Block.blocks.forEach(function (block) {
+                    block.draw(context);
+                });
+
+                if (queued) {
+                    queued = false;
+                    draw();
+                }
+
+                drawing = false;
+            });
         }
 
-        running = !running;
-    });
+
+        function requestDraw() {
+            if (drawing) {
+                queued = true;
+            } else {
+                draw();
+            }
+        }
+        exports.requestDraw = requestDraw;
+
+        return exports;
+    }());
 
 
-    start();
-    $tetris.removeClass('loading');
+    (function () {
+        var shape = new Shape(0, 0, 0);
+    }());
+
+    Control.start();
 }(jQuery));
