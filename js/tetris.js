894efe40e421f0c42fbdb9c53782b5e22380356b
Roland Warmerdam <rowno@webspirited.com>
2012-06-02 16:07:33 +1200

    Change to a grid based system and abstract the Shape object.

--- js/tetris.js
+++ js/tetris.js
@@ -6,11 +6,36 @@
 
     var CANVAS_WIDTH = 118,
         CANVAS_HEIGHT = 236,
-        PADDING = 2,
+        PADDING = 1,
         $tetris = $('#tetris'),
+        Grid,
         Score,
         Control,
         Render;
+
+
+    Grid = (function () {
+        var exports = {},
+            WIDTH = 12,
+            HEIGHT = 12;
+
+        exports.WIDTH = WIDTH;
+        exports.HEIGHT = HEIGHT;
+
+
+        function pixelX(x) {
+            return x * WIDTH;
+        }
+        exports.pixelX = pixelX;
+
+
+        function pixelY(y) {
+            return y * HEIGHT;
+        }
+        exports.pixelY = pixelY;
+
+        return exports;
+    }());
 
 
     function Block(color, x, y) {
@@ -22,28 +47,138 @@
     }
 
     Block.blocks = [];
-    Block.width = 10;
-    Block.height = 10;
 
     Block.prototype.draw = function (context) {
         context.fillStyle = this.color;
-        context.fillRect(this.x, this.y, Block.width, Block.height);
+        context.fillRect(
+            Grid.pixelX(this.x) + PADDING,
+            Grid.pixelY(this.y) + PADDING,
+            Grid.WIDTH - PADDING * 2,
+            Grid.HEIGHT - PADDING * 2
+        );
     };
 
 
-    function Shape(type, x, y, rotate) {
-        this.type = type;
+    function Shape() {}
+
+
+    function ShapeI(x, y) {
         this.x = x;
         this.y = y;
-        this.rotate = 0 || rotate;
+        this.rotation = 0;
 
         this.blocks = [
-            new Block('red', this.x, this.y),
-            new Block('red', this.x + Block.width + PADDING, this.y),
-            new Block('red', this.x + Block.width + PADDING, this.y + Block.height + PADDING),
-            new Block('red', this.x + (Block.width + PADDING) * 2, this.y + Block.height + PADDING)
+            new Block(this.COLOR, this.x, this.y),
+            new Block(this.COLOR, this.x + 1, this.y),
+            new Block(this.COLOR, this.x + 2, this.y),
+            new Block(this.COLOR, this.x + 3, this.y)
         ];
     }
+    ShapeI.prototype = new Shape();
+
+    ShapeI.prototype.COLOR = '#00FFFF';
+
+
+    function ShapeJ(x, y) {
+        this.x = x;
+        this.y = y;
+        this.rotation = 0;
+
+        this.blocks = [
+            new Block(this.COLOR, this.x, this.y),
+            new Block(this.COLOR, this.x + 1, this.y),
+            new Block(this.COLOR, this.x + 2, this.y),
+            new Block(this.COLOR, this.x + 2, this.y + 1)
+        ];
+    }
+    ShapeJ.prototype = new Shape();
+
+    ShapeJ.prototype.COLOR = '#0000FF';
+
+
+    function ShapeL(x, y) {
+        this.x = x;
+        this.y = y;
+        this.rotation = 0;
+
+        this.blocks = [
+            new Block(this.COLOR, this.x, this.y),
+            new Block(this.COLOR, this.x + 1, this.y),
+            new Block(this.COLOR, this.x + 2, this.y),
+            new Block(this.COLOR, this.x, this.y + 1)
+        ];
+    }
+    ShapeL.prototype = new Shape();
+
+    ShapeL.prototype.COLOR = '#FFA500';
+
+
+    function ShapeO(x, y) {
+        this.x = x;
+        this.y = y;
+        this.rotation = 0;
+
+        this.blocks = [
+            new Block(this.COLOR, this.x, this.y),
+            new Block(this.COLOR, this.x + 1, this.y),
+            new Block(this.COLOR, this.x, this.y + 1),
+            new Block(this.COLOR, this.x + 1, this.y + 1)
+        ];
+    }
+    ShapeO.prototype = new Shape();
+
+    ShapeO.prototype.COLOR = '#FFFF00';
+
+
+    function ShapeS(x, y) {
+        this.x = x;
+        this.y = y;
+        this.rotation = 0;
+
+        this.blocks = [
+            new Block(this.COLOR, this.x + 1, this.y),
+            new Block(this.COLOR, this.x + 2, this.y),
+            new Block(this.COLOR, this.x, this.y + 1),
+            new Block(this.COLOR, this.x + 1, this.y + 1)
+        ];
+    }
+    ShapeS.prototype = new Shape();
+
+    ShapeS.prototype.COLOR = '#00FF00';
+
+
+    function ShapeT(x, y) {
+        this.x = x;
+        this.y = y;
+        this.rotation = 0;
+
+        this.blocks = [
+            new Block(this.COLOR, this.x, this.y),
+            new Block(this.COLOR, this.x + 1, this.y),
+            new Block(this.COLOR, this.x + 2, this.y),
+            new Block(this.COLOR, this.x + 1, this.y + 1)
+        ];
+    }
+    ShapeT.prototype = new Shape();
+
+    ShapeT.prototype.COLOR = '#AA00FF';
+
+
+    function ShapeZ(x, y) {
+        var COLOR = '#FF0000';
+
+        this.x = x;
+        this.y = y;
+        this.rotation = 0;
+
+        this.blocks = [
+            new Block(COLOR, this.x, this.y),
+            new Block(COLOR, this.x + 1, this.y),
+            new Block(COLOR, this.x + 1, this.y + 1),
+            new Block(COLOR, this.x + 2, this.y + 1)
+        ];
+    }
+    ShapeZ.prototype = new Shape();
 
 
     Score = (function () {
@@ -170,7 +305,13 @@
 
 
     (function () {
-        var shape = new Shape(0, 0, 0);
+        var shapeI = new ShapeI(0, 0),
+            shapeJ = new ShapeJ(0, 2),
+            shapeL = new ShapeL(0, 5),
+            shapeO = new ShapeO(0, 8),
+            shapeS = new ShapeS(0, 11),
+            shapeT = new ShapeT(0, 14),
+            shapeZ = new ShapeZ(0, 17);
     }());
 
     Control.start();
