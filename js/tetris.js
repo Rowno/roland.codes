50eb7d7070d497972b88c16391d6251095739d33
Roland Warmerdam <rowno@webspirited.com>
2012-06-06 00:06:10 +1200

    Move the requestAnimationFrame polyfill to the top.

--- js/tetris.js
+++ js/tetris.js
@@ -39,12 +39,49 @@
         Control;
 
 
-    // Array.isArray polyfill
+    /**
+     * Array.isArray polyfill
+     */
     if (!Array.isArray) {
         Array.isArray = function (vArg) {
             return vArg.constructor === Array;
         };
     }
+
+
+    /**
+     * requestAnimationFrame polyfill
+     */
+    (function () {
+        var lastTime = 0,
+            vendors = ['ms', 'moz', 'webkit', 'o'],
+            x;
+
+        for (x = 0; x < vendors.length && !window.requestAnimationFrame; x += 1) {
+            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+            window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
+                                          window[vendors[x] + 'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame) {
+            window.requestAnimationFrame = function (callback, element) {
+                var currTime = new Date().getTime(),
+                    timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+                    id = window.setTimeout(
+                        function () { callback(currTime + timeToCall); },
+                        timeToCall
+                    );
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+        }
+
+        if (!window.cancelAnimationFrame) {
+            window.cancelAnimationFrame = function (id) {
+                clearTimeout(id);
+            };
+        }
+    }());
 
 
     Grid = (function () {
@@ -557,41 +594,6 @@
             context = $tetris.find('canvas').get(0).getContext('2d'),
             queued = false,
             drawing = false;
-
-
-        /**
-         * requestAnimationFrame polyfill
-         */
-        (function () {
-            var lastTime = 0,
-                vendors = ['ms', 'moz', 'webkit', 'o'],
-                x;
-
-            for (x = 0; x < vendors.length && !window.requestAnimationFrame; x += 1) {
-                window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-                window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
-                                              window[vendors[x] + 'CancelRequestAnimationFrame'];
-            }
-
-            if (!window.requestAnimationFrame) {
-                window.requestAnimationFrame = function (callback, element) {
-                    var currTime = new Date().getTime(),
-                        timeToCall = Math.max(0, 16 - (currTime - lastTime)),
-                        id = window.setTimeout(
-                            function () { callback(currTime + timeToCall); },
-                            timeToCall
-                        );
-                    lastTime = currTime + timeToCall;
-                    return id;
-                };
-            }
-
-            if (!window.cancelAnimationFrame) {
-                window.cancelAnimationFrame = function (id) {
-                    clearTimeout(id);
-                };
-            }
-        }());
 
 
         function draw() {
