08ce9b7265a45e130a4a816f64313cf59368df4d
Roland Warmerdam <rowno@webspirited.com>
2013-02-25 22:29:15 +1300

    Add javascript error tracking

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -9,6 +9,24 @@ requirejs.config({
 require(['jquery', 'app/variables', 'app/ga'], function ($) {
     'use strict';
 
+    // Track JavaScript errors in Google Analytics
+    (function (window, undefined) {
+        var link = function (href) {
+                var a = window.document.createElement('a');
+                a.href = href;
+                return a;
+            };
+        window.onerror = function (message, file, row) {
+            var host = link(file).hostname;
+            _gaq.push([
+                '_trackEvent',
+                (host === window.location.hostname || host === undefined || host === '' ? '' : 'external ') + 'error',
+                message, file + ' LINE: ' + row, undefined, undefined, true
+            ]);
+        };
+    }(window));
+
+
     $(function () {
         if (Modernizr.canvas) {
             $('#tetris .image').on('click', function () {
