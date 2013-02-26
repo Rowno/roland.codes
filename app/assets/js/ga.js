789e84c3621d641b62c339e2e89059c29803f21c
Roland Warmerdam <rowno@webspirited.com>
2013-02-26 20:13:45 +1300

    Use the optimized HTML5 Boilerplate GA tracking code

--- app/assets/js/ga.js
+++ app/assets/js/ga.js
@@ -1,18 +1,19 @@
 /*global _gaq:true */
 
-var _gaq = _gaq || [];
+var _gaq = [];
 
 define(['app/variables'], function (variables) {
     'use strict';
 
-    _gaq.push(['_setAccount', variables.googleAnalyticsID]);
-    _gaq.push(['_setSiteSpeedSampleRate', 100]);
-    _gaq.push(['_trackPageview']);
+    _gaq.push(
+        ['_setAccount', variables.googleAnalyticsID],
+        ['_setSiteSpeedSampleRate', 100],
+        ['_trackPageview']
+    );
 
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
+    (function (d, t) {
+        var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        g.src = ('https:' === location.protocol ? '//ssl':'//www') + '.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g, s);
+    }(document, 'script'));
 });
