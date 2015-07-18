a36505f763c4db5d82c5081c0057091087d10e44
Roland Warmerdam <rowno@webspirited.com>
2015-07-19 04:46:54 +1200

    Add smooth scrolling to hash links

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,7 +1,9 @@
 'use strict';
+require('./vendor/polyfills/smoothscroll');
 const THEME_NUM = 4;
 const THEME_INTERVAL = 30;
 let currentTheme = 1;
+
 
 setInterval(() => {
     document.documentElement.classList.remove('theme--' + currentTheme);
@@ -13,3 +15,20 @@ setInterval(() => {
 
     document.documentElement.classList.add('theme--' + currentTheme);
 }, THEME_INTERVAL * 1000);
+
+
+document.body.addEventListener('click', function (event) {
+    if (!event.target || !event.target.hash) {
+        return;
+    }
+
+    let element = document.querySelector(event.target.hash);
+
+    if (!element) {
+        return;
+    }
+
+    window.scrollTo({top: element.offsetTop, behavior: 'smooth'});
+    event.preventDefault(); // Prevent default scroll
+    window.location.hash = event.target.hash; // Manually add hash to url to maintain history
+}, false);
