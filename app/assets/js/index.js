5295809a8a63394cc4a80ab812a3b0eaed58f215
Roland Warmerdam <rowno@webspirited.com>
2015-07-19 00:36:58 +1200

    Add rotating theme

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,7 +1,15 @@
 'use strict';
+const THEME_NUM = 4;
+const THEME_INTERVAL = 10;
+let currentTheme = 1;
 
-class test {
-    constructor() {
+setInterval(() => {
+    document.documentElement.classList.remove('theme--' + currentTheme);
 
+    currentTheme += 1;
+    if (currentTheme > THEME_NUM) {
+        currentTheme = 1;
     }
-}
+
+    document.documentElement.classList.add('theme--' + currentTheme);
+}, THEME_INTERVAL * 1000);
