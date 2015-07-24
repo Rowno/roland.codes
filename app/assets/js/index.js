07dba30972961252ef034319987adcaa980040a1
Roland Warmerdam <rowno@webspirited.com>
2015-07-25 03:00:51 +1200

    Modularise theme

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,23 +1,8 @@
 'use strict';
 require('babel-core/polyfill');
 require('./vendor/polyfills/smoothscroll');
+require('./theme');
 require('./push-nav');
-const THEME_NUM = 4;
-const THEME_INTERVAL = 30;
-let currentTheme = 1;
-
-
-setInterval(() => {
-    document.documentElement.classList.remove('theme--' + currentTheme);
-
-    currentTheme += 1;
-    if (currentTheme > THEME_NUM) {
-        currentTheme = 1;
-    }
-
-    document.documentElement.classList.add('theme--' + currentTheme);
-}, THEME_INTERVAL * 1000);
-
 
 
 function isHashLink(node) {
---
+++ app/assets/js/theme.js
@@ -0,0 +1,16 @@
+'use strict';
+const THEME_NUM = 4;
+const THEME_INTERVAL = 30;
+let currentTheme = 1;
+
+
+setInterval(() => {
+    document.documentElement.classList.remove('theme--' + currentTheme);
+
+    currentTheme += 1;
+    if (currentTheme > THEME_NUM) {
+        currentTheme = 1;
+    }
+
+    document.documentElement.classList.add('theme--' + currentTheme);
+}, THEME_INTERVAL * 1000);
