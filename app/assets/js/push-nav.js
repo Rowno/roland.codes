35b0b9adba42e53f9dce2a0ace115f82fdf6541d
Roland Warmerdam <rowno@webspirited.com>
2015-07-28 08:17:53 +1200

    push-nav: only close the nav for hash links

--- app/assets/js/push-nav.js
+++ app/assets/js/push-nav.js
@@ -12,9 +12,24 @@ function togglePushNav() {
     pushNavActive = !pushNavActive;
 }
 
+function linkClick(event) {
+    const node = event.target;
+
+    // Only fire for hash links pointing at the current page
+    if (!node.hash ||
+        node.hostname !== window.location.hostname ||
+        node.pathname !== window.location.pathname)
+    {
+        return;
+    }
+
+    // Close nav when scrolling to a hash link target
+    togglePushNav();
+}
+
 
 document.querySelector('.push-nav-toggle').addEventListener('click', togglePushNav, false);
 document.querySelector('.push-nav-overlay').addEventListener('click', togglePushNav, false);
 for (let a of document.querySelectorAll('.push-nav a')) {
-    a.addEventListener('click', togglePushNav, false);
+    a.addEventListener('click', linkClick, false);
 }
