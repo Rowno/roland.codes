54bf5bd520faa4a223c2454bc4bdc56402adde1c
Roland Warmerdam <rowno@webspirited.com>
2015-10-01 18:12:52 +1300

    Lint the readme and gulp files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,14 +12,24 @@ require('./gulp/build');
 Gulp.task('clean', () => Del(Common.dest));
 
 Gulp.task('alex', () => {
-    return Gulp.src('app/**/*.md').pipe(Alex());
+    return Gulp.src([
+        'app/**/*.md',
+        'README.md',
+    ])
+    .pipe(Alex());
 });
 
 Gulp.task('lint', () => {
-    return Gulp.src(['app/**/*.js', '!app/**/vendor/**/*'])
-        .pipe(Eslint())
-        .pipe(Eslint.format())
-        .pipe(Eslint.failOnError());
+    return Gulp.src([
+        'app/**/*.js',
+        '!app/**/vendor/**/*',
+        '!app/static/demos/**/*',
+        'gulpfile.babel.js',
+        'gulp/**/*.js',
+    ])
+    .pipe(Eslint())
+    .pipe(Eslint.format())
+    .pipe(Eslint.failOnError());
 });
 
 Gulp.task('default', () => {
