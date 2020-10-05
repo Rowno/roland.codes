2263a4eea237ae2749444e20de8893ab3c1e6de7
Roland Warmerdam <rowno@webspirited.com>
2020-10-04 19:10:17 -0700

    Add font preloading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,11 @@ class CustomDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en" className="theme--1 preload">
-        <Head />
+        <Head>
+          <link rel="preload" href="/assets/fonts/roboto-thin.woff2" as="font" type="font/woff2" />
+          <link rel="preload" href="/assets/fonts/roboto-light.woff2" as="font" type="font/woff2" />
+        </Head>
+
         <body className="themed--bg">
           <Main />
           <NextScript />
