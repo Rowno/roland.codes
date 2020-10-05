26493b49fef25793f901c8e733d03bcdee06dc7e
Roland Warmerdam <rowno@webspirited.com>
2020-10-04 18:40:31 -0700

    Add missing preload class

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class CustomDocument extends Document {
   render(): JSX.Element {
     return (
-      <Html lang="en" className="theme--1">
+      <Html lang="en" className="theme--1 preload">
         <Head />
         <body className="themed--bg">
           <Main />
