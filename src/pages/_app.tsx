8d52199e3d57715544cd69520c86c4d7042f4282
Roland Warmerdam <rowno@webspirited.com>
2020-10-04 18:37:02 -0700

    Prevent transitions on page load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import 'highlight.js/styles/github.css'
 import '../css/index.scss'
 import '../rolee'
 
 const CustomApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Prevents transitions from triggering on page load
+    document.documentElement.classList.remove('preload')
+  }, [])
+
   return <Component {...pageProps} />
 }
 
