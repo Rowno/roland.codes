c5b1836c1d084de359fbaf7ef16f8070de9ccade
Roland Warmerdam <rowno@webspirited.com>
2020-11-24 00:41:34 -0800

    Simplify google analytics script injection

--- src/analytics.ts
+++ src/analytics.ts
@@ -15,15 +15,10 @@ if (typeof window !== 'undefined') {
     ga('send', 'pageview')
   }
 
-  const scripts = document.getElementsByTagName('script')
-  // Inject at the bottom of the page to avoid injecting next to the structured data script tags
-  // inside the React root and confusing React
-  const injectionPoint = scripts[scripts.length - 1]
   const script = document.createElement('script')
-  script.type = 'text/javascript'
   script.async = true
   script.src = 'https://www.google-analytics.com/analytics.js'
-  injectionPoint.parentNode?.insertBefore(script, injectionPoint)
+  document.body.appendChild(script)
 }
 
 export {}
